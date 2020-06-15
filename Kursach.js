35e7b2afd91e5a6bfaa655545753870d6a72941e
LovelyDrug <55019944+LovelyDrug@users.noreply.github.com>
2020-06-15 15:40:27 +0300

    Add files via upload

--- Kursach.js
+++ Kursach.js
@@ -8,6 +8,14 @@ header.set_title('Test');
 header.set_subtitle('Kursach');
 header.set_show_close_button(true);
 
+const win1 = new Gtk.Window();
+win1.window_position = Gtk.WindowPosition.CENTER;
+win1.connect('destroy', Gtk.main_quit);
+win1.set_default_size(750, 270);
+win1.set_titlebar(header);
+win1.border_width = 10;
+win1.show_all();
+
 const ListBoxRowWithData = Lang.Class({
     Name: "ListBoxRowWithData",
     Extends: Gtk.ListBoxRow,
@@ -19,27 +27,7 @@ const ListBoxRowWithData = Lang.Class({
     }
 });
 
-let app = new Gtk.Application({ application_id: 'org.gtk.advancedHello' });
 
-app.connect('activate', () => {
-log('App started');
-
-let mainWin = new Gtk.ApplicationWindow({ application: app });
-let container = new Gtk.Box({});
-
-let leftFrame = new Gtk.Frame({});
-let rightFrame = new Gtk.Frame({});
-
-container.add(leftFrame);
-container.add(rightFrame);
-
-leftFrame.set_size_request(500, 300);
-rightFrame.set_size_request(250, 300);
-
-mainWin.add(container);
-mainWin.show_all();
-});
-app.run([]);
 
 const ListBoxWindow = Lang.Class({
     Name: "ListBoxWindow",
@@ -87,20 +75,31 @@ const ListBoxWindow = Lang.Class({
         hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, spacing: 50});
         row.add(hbox);
         label = new Gtk.Label({label: "Date Format", xalign: 0});
+
         let combo = new Gtk.ComboBoxText();
         combo.insert(0, "0", "24-hour");
         combo.insert(1, "1", "AM/PM");
         hbox.pack_start(label, true, true, 0);
         hbox.pack_start(combo, false, true, 0);
 
-        listbox.add(row);
+       
 
         let listbox2 = new Gtk.ListBox();
-        let items = "Kyiv, Kharkiv, Lviv, Donetsk, Odessa, Dnipro, Zaporizhia, Kryvyi Rih, Mykolaiv, Mariupol, Luhansk, Sevastopol, Vinnytsia, Makiivka, Simferopol, Kherson, Poltava".split(',');
+        let items = ['Kyiv', 'Kharkiv', 'Lviv', 'Donetsk', 'Odessa', 'Dnipro', 'Zaporizhia', 'Mykolaiv', 'Mariupol', 'Luhansk', 'Sevastopol', 'Vinnytsia', 'Makiivka', 'Simferopol', 'Kherson', 'Poltava'];
+        let combo2 = new Gtk.ComboBoxText();
 
-        items.forEach(
-            item => listbox2.add(new ListBoxRowWithData(item))
-        );
+         listbox.add(row);
+        
+        row = new Gtk.ListBoxRow();
+        hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, spacing: 50});
+        row.add(hbox);
+        label = new Gtk.Label({label: "City", xalign: 0});
+        
+        items.forEach((item, index, array) => {
+            combo2.insert(index, " ${index} ", item);
+            hbox.pack_start(label, true, true, 0);
+            hbox.pack_start(combo2, false, true, 0);
+        });
 
         let sortFunc = function(row1, row2, data, notifyDestroy) {
             return row1.data.toLowerCase() > row2.data.toLowerCase();
@@ -110,8 +109,8 @@ const ListBoxWindow = Lang.Class({
             return (row.data != 'Fail');
         };
 
-        listbox2.set_sort_func(sortFunc, null, false);
-        listbox2.set_filter_func(filterFunc, null, false);
+        listbox2.set_sort_func(sortFunc);
+        listbox2.set_filter_func(filterFunc);
         
         listbox2.connect("row-activated", (widget, row) => print(row.data));
 
